fd64fb29969a6f09b4a1913cf6187e9f66984315
h-wata <hirofumi01.watanabe@g.softbank.co.jp>
2024-11-20 16:38:50 +0900

    Add intensity data handling in merge_frames_gpu
    Remove debugging code from merge_frames_gpu function

--- include/gtsam_points/types/gaussian_voxelmap_gpu.hpp
+++ include/gtsam_points/types/gaussian_voxelmap_gpu.hpp
@@ -79,9 +79,11 @@ public:
   int* num_points;               ///< Number of points in eac voxel
   Eigen::Vector3f* voxel_means;  ///< Voxel means
   Eigen::Matrix3f* voxel_covs;   ///< Voxel covariances
+  float* voxel_intensities;      ///< Voxel intensities
 };
 
 std::vector<Eigen::Vector3f> download_voxel_means(const GaussianVoxelMapGPU& voxelmap, CUstream_st* stream = nullptr);
 std::vector<Eigen::Matrix3f> download_voxel_covs(const GaussianVoxelMapGPU& voxelmap, CUstream_st* stream = nullptr);
+std::vector<float> download_voxel_intensities(const GaussianVoxelMapGPU& voxelmap, CUstream_st* stream = nullptr);
 
 }  // namespace gtsam_points
